7eb45e3320bda8413ef66cfec6994fbd067c5e97
Max Maeder <max.s.maeder@gmail.com>
2023-10-07 15:46:44 -0500

    Add tone test

--- frontend/app/tone/page.tsx
+++ frontend/app/tone/page.tsx
@@ -1,4 +1,6 @@
-// import Tone from 'tone';
+"use client";
+
+import * as Tone from 'tone'
 
 // //create a synth and connect it to the main output (your speakers)
 // const synth = new Tone.Synth().toDestination();
@@ -6,11 +8,18 @@
 // //play a middle 'C' for the duration of an 8th note
 // synth.triggerAttackRelease("C4", "8n");
 
-export default function Tone() {
+export default function TonePage() {
+    const play = () => {
+        const synth = new Tone.Synth().toDestination();
+        synth.triggerAttackRelease("C4", "8n");
+    }
+
 
     return (
         <main>
             <div>Test</div>
+
+            <button onClick={play}>Play</button>
         </main>
     );
 }
