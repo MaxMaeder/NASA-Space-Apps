b9548539cab74edf2fbd126605516d76936e897d
Max Maeder <max.s.maeder@gmail.com>
2023-10-07 14:37:55 -0500

    Sonification

--- python/sonification.py
+++ python/sonification.py
@@ -1,34 +1,43 @@
-
-import cv2 
-import numpy as np 
+import cv2
   
 # Read image. 
-img = cv2.imread("./python/my_plot.png", cv2.IMREAD_COLOR) 
+img = cv2.imread("./my_plot.png", cv2.IMREAD_COLOR) 
   
 # Convert to grayscale. 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
   
 # Blur using 3 * 3 kernel. 
-gray_blurred = cv2.blur(gray, (3, 3)) 
-  
-# Apply Hough transform on the blurred image. 
-detected_circles = cv2.HoughCircles(gray_blurred,  
-                   cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, 
-               param2 = 30, minRadius = 1, maxRadius = 40) 
-  
-# Draw circles that are detected. 
-if detected_circles is not None: 
-  
-    # Convert the circle parameters a, b and r to integers. 
-    detected_circles = np.uint16(np.around(detected_circles)) 
-  
-    for pt in detected_circles[0, :]: 
-        a, b, r = pt[0], pt[1], pt[2] 
-  
-        # Draw the circumference of the circle. 
-        cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
-  
-        # Draw a small circle (of radius 1) to show the center. 
-        cv2.circle(img, (a, b), 1, (0, 0, 255), 3) 
-        cv2.imshow("Detected Circle", img) 
-        cv2.waitKey(0) 
+img_blur = cv2.blur(img_gray, (3, 3)) 
+img_thresh = cv2.threshold(img_blur, 70, 255, cv2.THRESH_BINARY)[1]
+
+# Find contours
+contours, _ = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Contour lists
+contour_info = []
+min_size = 30
+kept_contours = []
+
+# Iterate through the contours
+for contour in contours:
+    # Calculate the x, y, and size of the bounding rectangle for the contour
+    x, y, w, h = cv2.boundingRect(contour)
+
+    # Skip small images
+    if w * h < min_size:
+        continue
+
+    # Append the x, y, and size to the list
+    kept_contours.append(contour)
+    contour_info.append((x, y, w, h))
+
+# Print the list of contour information
+for info in contour_info:
+    x, y, w, h = info
+    print(f"X: {x}, Y: {y}, Size (Width x Height): {w} x {h}")
+
+# Optionally, you can draw the contours on the original image for visualization
+cv2.drawContours(img, kept_contours, -1, (0, 255, 0), 2)
+cv2.imshow('Image with Contours', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
