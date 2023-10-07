f1826f43a6ffe9d29d0e772bdc7c5e46ba1f377c
Max Maeder <max.s.maeder@gmail.com>
2023-10-07 12:17:04 -0500

    Make background black

--- python/main.py
+++ python/main.py
@@ -8,8 +8,10 @@ plt.style.use(astropy_mpl_style)
 image_data = fits.getdata(image_file, ext=0)
 
 fig, ax = plt.subplots(figsize=(6, 6))
-ax.imshow(image_data, cmap="inferno", vmin=10, vmax=80)
+fig.set_facecolor('black')
 
+ax.imshow(image_data, cmap="inferno", vmin=10, vmax=80)
 ax.axis('off')
+
 plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
 plt.savefig('my_plot.png')
