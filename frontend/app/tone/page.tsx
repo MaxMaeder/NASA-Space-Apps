b761fc3b3589cfec6bb221679dea52764bc7f95b
jkintana <jkintana@protonmail.com>
2023-10-07 15:39:29 -0500

    Created tone testing page

---
+++ frontend/app/tone/page.tsx
@@ -0,0 +1,16 @@
+// import Tone from 'tone';
+
+// //create a synth and connect it to the main output (your speakers)
+// const synth = new Tone.Synth().toDestination();
+
+// //play a middle 'C' for the duration of an 8th note
+// synth.triggerAttackRelease("C4", "8n");
+
+export default function Tone() {
+
+    return (
+        <main>
+            <div>Test</div>
+        </main>
+    );
+}
