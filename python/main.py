a4740e12f2d360026986bafebccecc6572126935
Max Maeder <max.s.maeder@gmail.com>
2023-10-07 14:02:28 -0500

    Fix images

--- python/main.py
+++ python/main.py
@@ -2,16 +2,27 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 from astropy.visualization import astropy_mpl_style
 from astropy.utils.data import get_pkg_data_filename
+from pathlib import Path
 
-image_file = get_pkg_data_filename("sample.fits")
-plt.style.use(astropy_mpl_style)
-image_data = fits.getdata(image_file, ext=0)
+in_fits = list(Path("in").rglob("*.fits"))
 
-fig, ax = plt.subplots(figsize=(6, 6))
-fig.set_facecolor('black')
+for fits_path in in_fits:
+  fits_path = fits_path.as_posix()
 
-ax.imshow(image_data, cmap="inferno", vmin=10, vmax=80)
-ax.axis('off')
+  print("Processing '" + fits_path + "'...")
+  image_file = get_pkg_data_filename(fits_path)
 
-plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
-plt.savefig('my_plot.png')
+  plt.style.use(astropy_mpl_style)
+  header = fits.getheader(image_file)
+  image_data = fits.getdata(image_file, ext=0)
+
+  date_str = header["DATE_OBS"]
+
+  fig, ax = plt.subplots(figsize=(6, 6))
+  fig.set_facecolor("black")
+
+  ax.imshow(image_data, cmap="inferno", vmin=10, vmax=80)
+  ax.axis("off")
+
+  plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+  plt.savefig("out/" + date_str + ".png")
