896ee3dad28381c2cdb1a3e085811d1e7a5ff618
Max Maeder <max.s.maeder@gmail.com>
2023-10-07 12:15:01 -0500

    make img

--- python/main.py
+++ python/main.py
@@ -1,21 +1,15 @@
 from astropy.io import fits
-import numpy as np
-from matplotlib.colors import ListedColormap
-from matplotlib import cm
 import matplotlib.pyplot as plt
 from astropy.visualization import astropy_mpl_style
 from astropy.utils.data import get_pkg_data_filename
 
-image_file = get_pkg_data_filename('sample.fits')
+image_file = get_pkg_data_filename("sample.fits")
 plt.style.use(astropy_mpl_style)
 image_data = fits.getdata(image_file, ext=0)
-print(image_data.shape)
 
-plt.figure()
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.imshow(image_data, cmap="inferno", vmin=10, vmax=80)
 
-viridisBig = cm.get_cmap('plasma', 512)
-newcmp = ListedColormap(viridisBig(np.linspace(0.1, 0.75, 512)))
-
-plt.imshow(image_data, cmap=newcmp)
-plt.colorbar()
-plt.show()
+ax.axis('off')
+plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+plt.savefig('my_plot.png')
